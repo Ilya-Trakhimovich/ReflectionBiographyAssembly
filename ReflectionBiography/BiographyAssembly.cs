3c717b768951d34598f68b76fa9ed3b0c7a37ab0
Ilya_Trakhimovich <Ilya.Naroch@gmail.com>
2020-03-30 23:33:13 +0300

    Add project files.

---
+++ ReflectionBiography/BiographyAssembly.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace ReflectionBiography
+{
+    class BiographyAssembly
+    {
+        private Assembly _biographyAssembly;
+        private Type _concreteType;
+        private Type _attribute;
+
+        public Type ConcreteType
+        {
+            get => _concreteType; 
+        }
+
+        public BiographyAssembly(string path)
+        {
+            _biographyAssembly = Assembly.LoadFrom(path);
+        }
+
+        public void TypesOfAssembly()
+        {
+            var types = _biographyAssembly.GetTypes();
+
+            Console.WriteLine($"--- Types of {_biographyAssembly.FullName} ---");
+
+            foreach (var type in types)
+            {
+                Console.WriteLine(type.FullName);
+
+                if (type.Name == "InfoMetadataAttribute")
+                {
+                    _attribute = type;
+                }
+            }
+
+            Console.WriteLine();
+        }
+
+        public void SetConccreteType(string pathToType)
+        {
+            _concreteType = _biographyAssembly.GetType(pathToType, false, true);
+
+            object obj = Activator.CreateInstance(_concreteType);
+
+            var customAttribute = _concreteType.GetCustomAttribute(_attribute);
+
+        }
+    }
+}
---
+++ ReflectionBiography/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Reflection;
+using Lesson21;
+
+namespace ReflectionBiography
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly _biographyAssembly = Assembly.LoadFrom(@"C:\Users\пк\source\repos\Lesson21\Lesson21\bin\Debug\netcoreapp3.1\Lesson21.dll");
+
+            Console.WriteLine($"--- Types of Bigraphy Assembly ---");
+
+            var types = _biographyAssembly.GetTypes();
+
+            foreach (var type in types)
+            {
+                Console.WriteLine($"{type.Name}");
+            }
+
+            Console.WriteLine();
+
+            var biography = _biographyAssembly.GetType("Lesson21.Biography", false, true);
+            var objBiography = Activator.CreateInstance(biography);
+
+            var biographyFile = _biographyAssembly.GetType("Lesson21.BiographyFile", false, true);
+            var objBiographyFile = Activator.CreateInstance(biographyFile);
+
+            var attributes = biographyFile.GetCustomAttributes(false);
+
+            var typeAttribute = attributes[0].GetType();
+
+            Console.WriteLine($"--- Attributes of BiographyFile ---");
+
+            foreach (var attr in attributes)
+            {
+                Console.WriteLine(attr);
+            }
+
+            Console.WriteLine();
+
+            var properties = typeAttribute.GetProperties();
+
+            Console.WriteLine("--- Properties of InfoMetadataAttribute ---");
+
+            foreach (var property in properties)
+            {
+                Console.WriteLine($"{property.Name} - {property.GetValue(attributes[0])}");
+            }
+
+            Console.WriteLine();
+
+            var CompressOrDecompressBio = biographyFile.GetMethod("CompressOrDecompressBio");
+            CompressOrDecompressBio.Invoke(objBiographyFile, new object[] { objBiography, Mode.Deserialize});
+
+            var objBioProperty = biographyFile.GetProperty("Biography");
+
+            var GetBiography = biographyFile.GetMethod("GetBiography");
+            GetBiography.Invoke(objBiographyFile, new object[] { objBioProperty.GetValue(objBiographyFile)});
+        }
+    }
+}
